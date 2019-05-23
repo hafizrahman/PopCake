0f0e18f1dbed12fa3ca984cf04120abc51186e4e
hafizrahman <hfzrhmn@gmail.com>
2019-05-23 15:36:10 +0800

    Start service for network call.

---
+++ app/src/main/java/com/example/android/popcake/network/RecipeService.java
@@ -0,0 +1,11 @@
+package com.example.android.popcake.network;
+
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface RecipeService {
+}
