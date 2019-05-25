87ae58cfabe1ba81d7c5fdf155dfce586ff74c66
hafizrahman <hfzrhmn@gmail.com>
2019-05-25 23:01:10 +0800

    Start a Const class.

---
+++ app/src/main/java/com/example/android/popcake/Const.java
@@ -0,0 +1,6 @@
+package com.example.android.popcake;
+
+public class Const {
+    public static final String APP_TAG = "POPCAKES";
+    public static final String API_BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+}
