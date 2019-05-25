3c902c401c7cea49ef26139c7bffbead6d9e3a32
hafizrahman <hfzrhmn@gmail.com>
2019-05-25 23:02:55 +0800

    Add networking call. In theory this should be enough but it's not yet tested.

--- app/src/main/java/com/example/android/popcake/network/RecipeService.java
+++ app/src/main/java/com/example/android/popcake/network/RecipeService.java
@@ -8,4 +8,9 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface RecipeService {
+    // Check https://github.com/square/retrofit/issues/1567#issuecomment-180115150
+    // for the reason for the "./" usage. Basically we already have the full URL for the
+    // API and does not need query parameters.
+    @GET("./")
+    Call<JsonObject> getRecipes();
 }
--- app/src/main/java/com/example/android/popcake/repository/RecipeRepository.java
+++ app/src/main/java/com/example/android/popcake/repository/RecipeRepository.java
@@ -1,4 +1,50 @@
 package com.example.android.popcake.repository;
 
+import android.app.Application;
+import android.util.Log;
+
+import com.example.android.popcake.Const;
+import com.example.android.popcake.network.RecipeService;
+import com.google.gson.JsonObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RecipeRepository {
+    private RecipeService service;
+
+    public RecipeRepository(Application application) {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Const.API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        Call<JsonObject> mRecipes = service.getRecipes();
+        mRecipes.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                JsonObject mResponse = response.body();
+                // TODO: Do something with the response, save it to the database
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Log.d(Const.APP_TAG, "Network fail.");
+                t.printStackTrace();
+
+            }
+        });
+    }
 }
