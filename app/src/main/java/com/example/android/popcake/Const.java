1800b58ada7044b40f29f4b1b756446da1a8171b
hafizrahman <hfzrhmn@gmail.com>
2019-06-01 12:28:24 +0700

    Small cleanup.

--- app/src/main/java/com/example/android/popcake/Const.java
+++ app/src/main/java/com/example/android/popcake/Const.java
@@ -1,7 +1,7 @@
 package com.example.android.popcake;
 
 public class Const {
-    public static final String APP_TAG = "POPCAKES";
-    public static final String API_BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/";
-    public static final String API_JSON_FILENAME = "baking.json";
+    public static final String APP_TAG                  = "POPCAKES";
+    public static final String API_BASE_URL             = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/";
+    public static final String API_JSON_FILENAME        = "baking.json";
 }
--- app/src/main/java/com/example/android/popcake/network/RecipeService.java
+++ app/src/main/java/com/example/android/popcake/network/RecipeService.java
@@ -7,9 +7,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface RecipeService {
-    // Check https://github.com/square/retrofit/issues/1567#issuecomment-180115150
-    // for the reason for the "./" usage. Basically we already have the full URL for the
-    // API and does not need query parameters.
     @GET(Const.API_JSON_FILENAME)
     Call<JsonArray> getRecipes();
 }
