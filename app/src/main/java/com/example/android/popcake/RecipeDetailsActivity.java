e4c5313b4ad08b81287a2e38b89dee42fa445699
hafizrahman <hfzrhmn@gmail.com>
2019-06-06 16:29:06 +0700

    Code cleanup.

--- app/src/main/java/com/example/android/popcake/RecipeDetailsActivity.java
+++ app/src/main/java/com/example/android/popcake/RecipeDetailsActivity.java
@@ -16,17 +16,6 @@ public class RecipeDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
