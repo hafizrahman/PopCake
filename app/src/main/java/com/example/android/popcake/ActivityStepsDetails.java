5ad98a803b628b48118c814358124a5eb46541f8
hafizrahman <hfzrhmn@gmail.com>
2019-06-10 20:14:07 +0700

    More work for ViewPager to display swipeable list of steps' details.
    I am following https://developer.android.com/training/animation/screen-slide
    Had to add the belo specifically to the ActivityStepsDetails.java so
    the compiler doesn't complain:
    
    import androidx.fragment.app.FragmentStatePagerAdapter;

---
+++ app/src/main/java/com/example/android/popcake/ActivityStepsDetails.java
@@ -0,0 +1,65 @@
+package com.example.android.popcake;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentManager;
+
+public class ActivityStepsDetails extends AppCompatActivity {
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_steps_details);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.vp_activity_steps_details);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public FragmentRecipeStepDetails getItem(int position) {
+            return new FragmentRecipeStepDetails();
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+}
