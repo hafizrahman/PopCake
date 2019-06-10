ecdffa6eebcd761b22796baf43ab78c3370d18c2
hafizrahman <hfzrhmn@gmail.com>
2019-06-10 19:56:14 +0700

    Attempt: Use ViewPager to display swipeable list of steps' details.

---
+++ app/src/main/java/com/example/android/popcake/FragmentRecipeStepDetails.java
@@ -0,0 +1,32 @@
+package com.example.android.popcake;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android.popcake.model.Step;
+import com.example.android.popcake.viewmodel.ViewModelRecipe;
+
+import java.util.List;
+
+public class FragmentRecipeStepDetails extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_recipe_step_details, container, false);
+        return rootView;
+    }
+
+}
