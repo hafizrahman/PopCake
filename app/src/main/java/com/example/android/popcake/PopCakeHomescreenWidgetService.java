80e6a37022f5cc1af35301116cb7f596dbb5590e
hafizrahman <hfzrhmn@gmail.com>
2019-06-15 14:29:44 +0700

    Apparently this is all that's needed in the RemoteViewService.

--- app/src/main/java/com/example/android/popcake/PopCakeHomescreenWidgetService.java
+++ app/src/main/java/com/example/android/popcake/PopCakeHomescreenWidgetService.java
@@ -1,6 +1,11 @@
 package com.example.android.popcake;
 
+import android.content.Intent;
 import android.widget.RemoteViewsService;
 
 public class PopCakeHomescreenWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new PopCakeHomescreenWidgetDataProvider(this, intent);
+    }
 }
